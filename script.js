0ba5b9ad04151a06f4979ea245d2858a1fa77080
Khoi <147691737+thienkhoinguyen@users.noreply.github.com>
2024-03-30 00:55:00 -0600

    Update script.js
    set up api but not full list

--- script.js
+++ script.js
@@ -15,3 +15,46 @@ async function createCardElement(item) {
          </div>
     </li>`;
 }
+
+async function fetchDigimonDetails(url) {
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+async function fetchDigimonList() {
+    try {
+        const response = await fetch("https://digi-api.com/api/v1/digimon");
+        const data = await response.json();
+        nextPageUrl = data.pageable.nextPage;
+        return data.content;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+async function renderOption1Dropdown() {
+    const select = document.getElementById("dropdown");
+    let list = await fetchDigimonList();
+    console.log(list);
+    while (list) {
+        list.forEach((item) => {
+            const option = document.createElement("option");
+            option.textContent = item.name;
+            option.value = item.href;
+            select.appendChild(option);
+        });
+        list = await fetchMoreDigimon();
+    }
+}
+
+renderOption1Dropdown();
+
+const option1SubmitButton = document.getElementById("submit-button");
+option1SubmitButton.addEventListener("click", option1DropdownClickHandler);
